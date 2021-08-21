cfcf8006dcdaf62e10b084794ebc61cef237ec1b
gopal2812 <er.gopalgupta@gmail.com>
2021-08-21 19:26:39 +0530

    initial commit

---
+++ hello.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+# Create a Flask Instance
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
+app.config['EXPLAIN_TEMPLATE_LOADING'] = True
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+	name = StringField("What's Your Name", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+
+# Create a route decorator
+@app.route('/')
+
+#def index():
+#	return "<h1>Hello World!</h1>"
+
+# FILTERS!!!
+#safe
+#capitalize
+#lower
+#upper
+#title
+#trim
+#striptags
+
+
+def index():
+	first_name = "Gopal Gupta"
+	stuff = "This is a capastone project"
+	Learning_concept = ["Advance Python", "Flask", "webhosting"]
+	return render_template("index.html", 
+		first_name=first_name,
+		stuff=stuff,
+		Learning_concept = Learning_concept)
+
+# localhost:5000/user/Gopal
+@app.route('/user/<name>')
+
+def user(name):
+	return render_template("user.html", user_name=name)
+
+# Create Custom Error Pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template("500.html"), 500
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+	name = None
+	form = NamerForm()
+	# Validate Form
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+		
+	return render_template("name.html", 
+		name = name,
+		form = form)
+
